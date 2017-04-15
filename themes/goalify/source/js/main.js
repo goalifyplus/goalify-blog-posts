de629985c52ddc39997c59752d64de2cc7833d97
Eric Tran <tung.tran@naustud.io>
2017-04-15 23:32:25 +0700

    add files from nau16

---
+++ themes/goalify/source/js/main.js
@@ -0,0 +1,13 @@
+/* © 2017 NauStud.io
+ * @author Eric
+ */
+
+var goalify = goalify || {};
+
+(function() {
+	'use strict';
+	var heroBanner = goalify.HeroBanner;
+	var navBarToggle = goalify.NavBarToggleWithBackground;
+	var getInTouch = goalify.GetInTouchAnimation;
+
+}());
